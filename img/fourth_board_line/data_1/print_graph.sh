aaedaa38b9065da6b607b56e807d8c16405f57ef
skoworoik <skowroik@live.it>
2019-12-07 17:31:13 +0100

    Another

---
+++ img/fourth_board_line/data_1/print_graph.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/gnuplot -persist
+file = 'calib_10'
+type_out = '.png'
+output = file.type_out
+f(x) = a + b*x
+fit f(x) file using 1:2:3 via a, b
+set xrange [0:1.6]
+set xlabel 'Voltage (V)'
+set ylabel 'Channel'
+set grid
+set key left
+set terminal pngcairo
+set output output
+set xzeroaxis
+plot file using 1:( $2 - f($1) ):3 with yerrorbars title 'Data'
+exit
---
+++ img/plot/cm/print_graph.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/gnuplot -persist
+n = '12'
+file = '_res_cm'
+type_out = '.png'
+cable = 'Cable '
+title_plot = cable.n
+file_name = n.file
+output = n.file.type_out
+set xlabel 'Channel Number'
+set ylabel 'Resolution (%)'
+set grid
+set offsets 1, 1, 0.1, 0.1
+set terminal pngcairo
+set output output
+plot file_name using 1:2:3 with yerrorbars title title_plot 
+exit
